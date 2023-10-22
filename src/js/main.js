bd45ab5c08cbe9b3d82e0a610c54d29672f7e268
Ilya <ilieturcan424@gmail.com>
2023-10-22 17:50:56 +0300

    commit

--- src/js/main.js
+++ src/js/main.js
@@ -105,16 +105,12 @@ window.addEventListener("DOMContentLoaded", () => {
     //Modal
 
     const btn = document.querySelectorAll("[data-modal]"),
-        modal = document.querySelector(".modal"),
-        close = document.querySelector("[data-close]");
+        modal = document.querySelector(".modal");
     
     function modalOpen() {
-        btn.forEach(item => {
-            item.addEventListener("click", () => {
-                modal.classList.add("show");
-                document.body.style.overflow = "hidden"; // Не позволяет прокручиваться странице
-            });
-        });
+        modal.classList.add("show");
+        document.body.style.overflow = "hidden"; // Не позволяет прокручиваться странице
+        clearInterval(modalTimerId);
     }
 
     function modalClose() {
@@ -122,11 +118,12 @@ window.addEventListener("DOMContentLoaded", () => {
         document.body.style.overflow = "";
     }
 
-    modalOpen();
-    close.addEventListener("click", modalClose);
+    btn.forEach(item => {
+        item.addEventListener("click", modalOpen);
+    });
 
     modal.addEventListener("click", (e) => {
-        if (e.target === modal) {
+        if (e.target === modal || e.target.getAttribute("data-close") == "") {
             modalClose();
         }
     });
@@ -134,6 +131,248 @@ window.addEventListener("DOMContentLoaded", () => {
     document.addEventListener("keydown", (e) => {
         if (e.code === "Escape" && modal.classList.contains("show")) {
             modalClose();
-        }
+        }   
     });
+
+    const modalTimerId = setTimeout(modalOpen, 50000);
+
+    function showModalByScroll() {
+        if (window.scrollY + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1) {
+            modalOpen();
+            window.removeEventListener("scroll", showModalByScroll);
+        }
+    }
+
+    window.addEventListener("scroll", showModalByScroll);
+
+    // Используем классы для карточек
+
+    class MenuCard {
+        constructor(src, alt, title, descr, price, parentSelector, ...classes) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.classes = classes;
+            this.parentSelector = document.querySelector(parentSelector);
+            this.transfer = 27;
+            this.changeToUAH();
+        }
+
+        changeToUAH() {
+            this.price = this.price * this.transfer;
+        }
+
+        render() {
+            const element = document.createElement("div");
+
+            if (this.classes.length == 0) {
+                this.classes = "menu__item";
+                element.classList.add(this.classes);
+            } else {
+                this.classes.forEach(className => element.classList.add(className));
+            }
+
+            element.innerHTML = `
+                    <img src=${this.src} alt=${this.alt}>
+                    <h3 class="menu__item-subtitle">${this.title}</h3>
+                    <div class="menu__item-descr">${this.descr}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                    </div>
+                `;
+            this.parentSelector.append(element); 
+        }
+    }
+
+    // const div = new MenuCard();
+    // div.render();
+
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        "Меню \"Фитнес\"",
+        "Меню \"Фитнес\" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов.Продукт активных и здоровых людей.Это абсолютно новый продукт с оптимальной ценой и высоким качеством!",
+        9,
+        ".menu .container",
+        "menu__item",
+        "big"
+    ).render();
+
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        "Меню \"Фитнес\"",
+        "Меню \"Фитнес\" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов.Продукт активных и здоровых людей.Это абсолютно новый продукт с оптимальной ценой и высоким качеством!",
+        9,
+        ".menu .container",
+        "menu__item"
+    ).render();
+
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        "Меню \"Фитнес\"",
+        "Меню \"Фитнес\" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов.Продукт активных и здоровых людей.Это абсолютно новый продукт с оптимальной ценой и высоким качеством!",
+        9,
+        ".menu .container",
+        "menu__item"
+    ).render();
+
+    //Forms
+
+    const forms = document.querySelectorAll("form");
+    console.log(forms);
+    const message = {
+        loading: "img/form/spinner.svg",
+        success: "Спасибо, скоро мы с вами свяжимся",
+        failure: "Что-то пошло не так..."
+    };
+
+    forms.forEach(item => {
+        postData(item);
+    });
+
+    function postData(form) {
+        form.addEventListener("submit", (e) => {
+            e.preventDefault();
+
+            const statusMessage = document.createElement("img");
+            statusMessage.src = message.loading;
+            statusMessage.style.cssText = `
+                display: block;
+                margin: 0 auto;
+            `; // в него можем записать css стили×
+            form.insertAdjacentElement("afterend", statusMessage); // Позволяет помещать наши элементы в разные места верстки. Используем вместо append. 1 аргумент это куда вставляем элемент. afterend - после формы, 2 аргумент - то, что нам нужно вставить.
+            //Обычный PHP метод
+            
+            // const request = new XMLHttpRequest();
+            // request.open("POST", "server.php");
+
+            // // request.setRequestHeader("Content-type", "multipart/form-data"); При связке XMLHttpRequest() и FormData, заголовок не нужен
+            
+            // const formData = new FormData(form);
+
+            // request.send(formData);
+
+            //JSON
+
+            // const request = new XMLHttpRequest();
+            // request.open("POST", "server.php");
+
+            //  request.setRequestHeader("Content-type", "application/json");
+
+            // const formData = new FormData(form); // Собираем все данные из нашей формы.
+
+            // const object = {};
+
+            // formData.forEach((value, key) => {
+            //     object[key] = value;
+            // });
+
+            // const json = JSON.stringify(object);
+
+            // request.send(json);
+
+            // request.addEventListener("load", () => { // когда наш запрос полностью завершится 
+            //     if (request.status === 200) {
+            //         console.log(request.response);
+            //         showThanksModal(message.success);
+            //         form.reset(); // Очистка формы
+            //         statusMessage.remove();
+            //     } else {
+            //         showThanksModal(message.failure);
+            //     }
+            // });
+            
+            // Обычный PHP метод
+            
+            // const formData = new FormData(form);
+            
+            // fetch("server.php", { // Отправляем наши данные.
+            //     method: "POST",
+            //     body: formData
+            // }).then(data => data.text()) // переделываем наш ответ в обычный текст.
+            //     .then(data => { // Обрабатываем результат нашего запроса
+            //         console.log(data);
+            //         showThanksModal(message.success);
+            //         statusMessage.remove();
+            //     }).catch(() => {
+            //         showThanksModal(message.failure);
+            //     }).finally(() => {
+            //         form.reset(); // Очистка формы
+            //     });
+            
+            
+            // JSON метод   
+            const formData = new FormData(form);
+
+            const object = {};
+
+            formData.forEach((value, key) => {
+                object[key] = value;
+            });
+
+            // const json = JSON.stringify(object);    
+            
+            fetch("server.php", { // Отправляем наши данные.
+                method: "POST",
+                headers: {
+                    "Content-type": "application/json"
+                },
+                body: JSON.stringify(object)
+            }).then(data => data.text()) // переделываем наш ответ в обычный текст.
+                .then(data => { // Обрабатываем результат нашего запроса
+                    console.log(data);
+                    showThanksModal(message.success);
+                    statusMessage.remove();
+                }).catch(() => {
+                    showThanksModal(message.failure);
+                }).finally(() => {
+                    form.reset(); // Очистка формы
+                });
+        });
+    }
+
+    function showThanksModal(message) {
+        const prevModalDialog = document.querySelector(".modal__dialog");
+
+
+        prevModalDialog.classList.add("hide");
+        modalOpen();
+
+        const thanksModal = document.createElement("div");
+        thanksModal.classList.add("modal__dialog");
+        thanksModal.innerHTML = `<div class = "modal__content"> 
+                                    <div data-close class="modal__close">×</div>
+                                    <div class="modal__title">${message}</div>
+                                </div>`;
+        
+        document.querySelector(".modal").append(thanksModal);
+
+        setTimeout(() => {
+            thanksModal.remove();
+            prevModalDialog.classList.add("show");
+            prevModalDialog.classList.remove("hide");
+            modalClose();
+        }, 4000);
+    }
+
+    // fetch("https://jsonplaceholder.typicode.com/posts", { // Классический post запрос 
+    //     method: "POST",
+    //     body: JSON.stringify({name: "Alex"}), // Тут может быть строка или объект
+    //     headers: {
+    //         "Content-type": "application/json" 
+    //     }
+    // })
+    //     .then(response => response.json())
+    //     .then(json => console.log(json));
+    
+    // fetch("https://jsonplaceholder.typicode.com/posts/1") // Классический get запрос.
+    //     .then((response) => response.json()) // обрабатывает данные сервера, чтобы мы могли их использовать. response.json() это альтернаьтива parse.
+    //     .then((json) => console.log(json)); // Выводим наш объект.
 });
+
